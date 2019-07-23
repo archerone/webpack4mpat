fc4ec23891f7d0a353bf4559bd3df70955d1f090
袁超 <yuanchao@yuanchaodeMac-mini.local>
2019-07-23 17:47:10 +0800

    m8

--- src/index/index.js
+++ src/index/index.js
@@ -3,7 +3,7 @@ import utils from '../../util/pa.js';
 import treesk from '../../util/treesk.js';
 document.write(helloworld());
 utils.pa();
-console.log($("img").length,'ddcd')
+console.log($("img").length,'ddcd');
 $('body').click(function(){
   import(/* webpackChunkName: "pb" */ '../../util/pb.js').then(mod=>{
     mod.default.pb();
